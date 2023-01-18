ca08ee27bd653ae5d825f63c6ca4a2347e6ece16
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-01-18 15:35:57 -0500

    finished basic structure

--- src/core/commands/dm.js
+++ src/core/commands/dm.js
@@ -1,18 +1,31 @@
 const client = require("../index");
 
+const data = require("../../data.json");
+const { sendImage } = require("../../utils");
+
 module.exports.run = () => {
-  const numero = document
-  .getElementById("testNumber")
-  .value;
-  if(numero === '' || isNaN(numero)) return alert("Insira um número válido.")
-  const testNumber = `${numero.replaceAll("+", "")
-      .replaceAll(" ", "")
-      .replaceAll("-", "")
-      .replaceAll("(", "")
-      .replaceAll(")", "")}@c.us`
-  client.sendMessage(
-    testNumber,
-    "Essa é uma mensagem de teste do painel do Prospect BOT"
-  );
-  alert("Enviado.")
+  const numero = document.getElementById("testNumber").value;
+  if (numero === "") return alert("Insira um número válido.");
+  const testNumber = `${numero
+    .replaceAll("+", "")
+    .replaceAll(" ", "")
+    .replaceAll("-", "")
+    .replaceAll("(", "")
+    .replaceAll(")", "")}@c.us`;
+
+  const files = [];
+
+  fs.readdir(`${__dirname}/../../medias`, (err, filesLoaded) => {
+    filesLoaded.forEach((file) => {
+      files.push(`${__dirname}/../../medias/${file}`);
+    });
+
+    client.sendMessage(testNumber, `${data.text}`);
+
+    files.forEach(async (f) => {
+      await sendImage(client, testNumber, "", f);
+    });
+
+    alert("Enviado.");
+  });
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,19 @@
 const fs = require("fs");
+const { MessageMedia } = require("whatsapp-web.js");
 
 module.exports = {
-    saveData: (data) => {
-        const toStringData = JSON.stringify(data);
-        fs.writeFileSync(`${__dirname}/../data.json`, toStringData);
-        alert("Suas preferências foram salvas.");
-      }
-}
+  saveData: (data) => {
+    const toStringData = JSON.stringify(data);
+    fs.writeFileSync(`${__dirname}/../data.json`, toStringData);
+    alert("Suas preferências foram salvas.");
+  },
+  sendImage: async (client, phone, caption = "", path) => {
+    return new Promise(async function (resolve, reject) {
+      const media = MessageMedia.fromFilePath(path);
+      client
+        .sendMessage(phone, media, { caption })
+        .then((res) => resolve("Succesfully sent."))
+        .catch((error) => reject("Can not send message.", error));
+    });
+  },
+};
