683aa10e9f01957351f5e965c4b7cf396837ae99
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-01-18 09:04:46 -0500

    [added] auto reloader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
+const electronReload = require('electron-reload');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -96,3 +97,4 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+electronReload(__dirname);
