56d8a0d644e2440ef6c8778f5e4e2cf48a3b8381
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-01-18 15:39:28 -0500

    formated code

--- src/core/commands/pem.js
+++ src/core/commands/pem.js
@@ -1,107 +1,104 @@
-const SerpApi = require('google-search-results-nodejs')
-const {text, type, location, apiKey} = require("../../data.json")
-const search = new SerpApi.GoogleSearch(apiKey)
-const fs = require('fs');
+const SerpApi = require("google-search-results-nodejs");
+const { text, type, location, apiKey } = require("../../data.json");
+const search = new SerpApi.GoogleSearch(apiKey);
+const fs = require("fs");
 const db = require("../../db.json");
 
 module.exports.run = async (client, message) => {
+  if (!text || !type || !location) return;
 
-    if(!text || !type || !location) return;
+  const chats = await client.getAllChats();
+  const chatNumbers = db.numbers || [];
 
-    const chats = await client.getAllChats();
-    const chatNumbers = db.numbers || []
+  chats.forEach((c) => {
+    chatNumbers.push(c.id._serialized);
+  });
 
-    chats.forEach(c => {
-        chatNumbers.push(c.id._serialized)
+  console.log(chatNumbers);
+
+  const q = `${type} ${location}`;
+
+  const numbers = [],
+    removed = [],
+    files = [];
+
+  fs.readdir(`${__dirname}/../../medias`, (err, filesLoaded) => {
+    console.log(filesLoaded);
+    filesLoaded.forEach((file) => {
+      files.push(`${__dirname}/../../medias/${file}`);
     });
 
-    console.log(chatNumbers);
+    console.log(`Encontrei ${files.length} para envio.`);
 
-    const q = `${type} ${location}`
+    const config = {
+      pages: 25,
+      intervaloEnvio: 5,
+    };
 
-    const numbers = [],
-        removed = [],
-        files = [];
+    client.sendMessage(message.from, `*Pesquisando...*`);
 
-    fs.readdir(`${__dirname}/../../medias`, (err, filesLoaded) => {
-        console.log(filesLoaded)
-        filesLoaded.forEach(file => {
-            files.push(`${__dirname}/../../medias/${file}`)
-        });
-
-        console.log(`Encontrei ${files.length} para envio.`);
-
-        const config = {
-            pages: 25,
-            intervaloEnvio: 5
+    for (let index = 0; index < config.pages; index++) {
+      search.json(
+        {
+          engine: "google_maps",
+          type: "search",
+          ll: "@-10.3488815,-58.9089475,5.6z",
+          start: index === 0 ? 0 : (index + 1) * 20,
+          q,
+        },
+        (result) => {
+          if (result.local_results === undefined) return;
+          result.local_results.forEach((i) => {
+            if (i.phone === undefined) return;
+            const formated = i.phone
+              .replaceAll("+", "")
+              .replaceAll(" ", "")
+              .replaceAll("-", "")
+              .replaceAll("(", "")
+              .replaceAll(")", "");
+            if (formated.slice(4).startsWith("9")) {
+              if (chatNumbers.includes(`${formated}@c.us`)) {
+                removed.push(`${formated}@c.us`);
+              } else {
+                numbers.push(`${formated}@c.us`);
+              }
+            }
+          });
         }
+      );
+    }
 
-        client.sendMessage(
-            message.from,
-            `*Pesquisando...*`
-        );
+    setTimeout(() => {
+      if (numbers.length === 0)
+        return alert("Ocorreu um problema com a pesquisa.");
 
-        for (let index = 0; index < config.pages; index++) {
-            search.json({
-                engine: "google_maps",
-                type: "search",
-                ll: '@-10.3488815,-58.9089475,5.6z',
-                start: index === 0 ? 0 : (index + 1) * 20,
-                q,
-            }, (result) => {
-                if (result.local_results === undefined) return;
-                result.local_results.forEach(i => {
-                    if (i.phone === undefined) return;
-                    const formated = i.phone.replaceAll("+", "").replaceAll(" ", "").replaceAll("-", "")
-                    if (formated.slice(4).startsWith("9")) {
-                        if (chatNumbers.includes(`${formated}@c.us`)) {
-                            removed.push(`${formated}@c.us`);
-                        } else {
-                            numbers.push(`${formated}@c.us`);
-                        }
-                    }
-                });
+      alert(
+        `Coletei *${numbers.length}* números válidos de empresas. ${
+          removed.length
+        } foram removidos porque estão repetidos.\n\nConfiguração do BOT: \n\nLimite de buscas: *${
+          config.pages * 20
+        } empresas* || Intervalo de envio: *${
+          config.intervaloEnvio
+        }s*\n\n\n*⚠️Realizando envio para os números⚠️*`
+      );
+
+      numbers.forEach((num) => {
+        setTimeout(async () => {
+          try {
+            client.sendMessage(num, text.toString());
+            files.forEach(async (f) => {
+              await sendImage(client, num, "", f);
             });
-        }
-
-        setTimeout(() => {
-
-            if (numbers.length === 0) return client.sendMessage(
-                message.from,
-                `Ocorreu um problema com a pesquisa.`
-            );
-
-            client.sendMessage(
-                message.from,
-                `Coletei *${numbers.length}* números válidos de empresas. ${removed.length} foram removidos porque estão repetidos.\n\nConfiguração do BOT: \n\nLimite de buscas: *${config.pages * 20} empresas* || Intervalo de envio: *${config.intervaloEnvio}s*\n\n\n*⚠️Realizando envio para os números⚠️*`
-            );
-
-            numbers.forEach(num => {
-                setTimeout(async () => {
-                    try {
-                        client.sendMessage(
-                            num,
-                            text.toString()
-                        );
-                        files.forEach(async f => {
-                            await client
-                                .sendFile(
-                                    num,
-                                    f,
-                                    '',
-                                    ''
-                                )
-                        });
-                    } catch (error) {
-                        console.log(error)
-                    }
-                }, config.intervaloEnvio * 1000)
-            })
-        }, 10 * 1000);
-        const save = {
-            numbers: chatNumbers
-        }
-        const toStringData = JSON.stringify(save);
-        fs.writeFileSync(`${__dirname}/../../db.json`, toStringData);
-    });
-}
+          } catch (error) {
+            console.log(error);
+          }
+        }, config.intervaloEnvio * 1000);
+      });
+    }, 10 * 1000);
+    const save = {
+      numbers: chatNumbers,
+    };
+    const toStringData = JSON.stringify(save);
+    fs.writeFileSync(`${__dirname}/../../db.json`, toStringData);
+  });
+};
