cd212c1fb052d9e534d3794448a8573677dadb4b
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-01-18 07:43:54 -0500

    first

---
+++ src/pages/index.js
@@ -0,0 +1,61 @@
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const client = new Client({
+  restartOnAuthFail: true,
+  authStrategy: new LocalAuth(),
+});
+
+const genqr = require("qr-image");
+
+const qrElement = document.getElementById("qr");
+const divInitial = document.getElementById("divInitial");
+const mainElement = document.getElementById("main");
+const nameElement = document.getElementById("namebot");
+
+//buttons
+const infoSave = document.getElementById("infoSave");
+const configSave = document.getElementById("configSave");
+
+client.on("qr", (qr) => {
+  divInitial.innerHTML = `<p>Para que o bot tenha acesso ao seu whatsapp, você precisa ler o <b>código QR</b>.</p>`;
+  const qrImage = genqr.image(qr);
+  const chunks = [];
+  qrImage.on("data", (chunk) => chunks.push(chunk));
+  qrImage.on("end", () => {
+    const buffer = Buffer.concat(chunks);
+    const base64data = buffer.toString("base64");
+    qrElement.src = `data:image/png;base64,${base64data}`;
+  });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+  divInitial.style.display = "none";
+  qrElement.style.display = "none";
+  namebot.innerHTML = `Prospect bot <span class="badge text-bg-success">Online</span>`;
+  mainElement.style.display = "block";
+});
+
+client.on("message", (message) => {
+  if (message.body === "!ping") {
+    client.sendMessage(message.from, "Dados coletados.");
+    console.log(message);
+  }
+});
+
+client.on("disconnected", () => {
+  mainElement.style.display = "none";
+  namebot.innerHTML = `Prospect bot <span class="badge text-bg-danger">Desconectado</span>`;
+  setTimeout(() => {
+    location.reload();
+  }, 3000);
+});
+
+infoSave.addEventListener("click", () => {
+  alert("Botao");
+});
+
+configSave.addEventListener("click", () => {
+  alert("Botao");
+});
+
+client.initialize();
