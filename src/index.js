aa284711553e4b049d678c7b1d4804828400a3f0
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-02-03 00:43:30 -0500

    [added] verify key on databse

--- src/index.js
+++ src/index.js
@@ -3,10 +3,30 @@ const path = require('path');
 // const electronReload = require('electron-reload');
 const {production} = require('../config.json');
 const fs = require('fs');
+const {initializeApp} = require('@firebase/app');
+const {getDatabase, ref, onValue, get, child} = require('@firebase/database');
+require('dotenv').config();
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
+}
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+};
+
+try {
+  initializeApp(firebaseConfig);
+  console.log('Connected to Firebase Database');
+} catch (error) {
+  console.error(error);
 }
 
 ipcMain.on('notification', (event, arg) => {
@@ -74,6 +94,53 @@ if (!production) {
   });
 }
 
+let verificationWindow;
+// eslint-disable-next-line require-jsdoc
+function generateLicenseKey() {
+  const key = [];
+
+  for (let i = 0; i < 4; i++) {
+    const part = [];
+    for (let j = 0; j < 5; j++) {
+      part.push(Math.random().toString(36).substring(2, 3).toUpperCase());
+    }
+    key.push(part.join(''));
+  }
+
+  return key.join('-');
+}
+let licenseKey;
+
+try {
+  licenseKey = fs.readFileSync('license.key', 'utf-8');
+} catch (error) {
+  licenseKey = generateLicenseKey();
+  fs.writeFileSync('license.key', licenseKey);
+}
+
+// eslint-disable-next-line require-jsdoc
+function createVerificationWindow() {
+  verificationWindow = new BrowserWindow({
+    width: 400,
+    height: 300,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    icon: __dirname + '/assets/icon.png',
+  });
+
+  verificationWindow.setMenu(null);
+
+  verificationWindow.loadFile(`${__dirname}/pages/verification.html`);
+
+  verificationWindow.once('ready-to-show', () => {
+    verificationWindow.show();
+    verificationWindow.webContents.send('key', licenseKey);
+  });
+}
+
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
@@ -94,7 +161,35 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  /* const db = getDatabase();
+  const keys = ref(db, 'prospect/keys');
+   onValue(
+        keys,
+        (snapshot) => {
+          if (snapshot.exists() && snapshot.val()) {
+            console.log(snapshot.val());
+            createWindow();
+          } else {
+            createVerificationWindow();
+          }
+        },
+        {
+          onlyOnce: true,
+        },
+    );*/
+  const dbRef = ref(getDatabase());
+  get(child(dbRef, `prospect/keys/${licenseKey}`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+    } else {
+      createVerificationWindow();
+    }
+  }).catch((error) => {
+    console.error(error);
+    createVerificationWindow();
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
