75d041cb3f00514e4af1204d5ac8f4e5e8e591ce
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-02-28 20:40:04 -0500

    upgrade build.bat

--- build.bat
+++ build.bat
@@ -5,11 +5,18 @@ echo Bem-vindo ao script de compilação Prospect!
 color 07
 echo Procurando por arquivos e pastas desnecessários...
 
-IF EXIST license.json (
+IF EXIST dist (
     color 0C
-    echo Removendo o arquivo license.json...
+    echo Removendo a pasta dist...
     color 07
-    del /f /q license.json
+    rd /s /q dist
+)
+
+IF EXIST license.key (
+    color 0C
+    echo Removendo o arquivo license.key...
+    color 07
+    del /f /q license.key
 )
 
 IF EXIST Prospect-installers (
@@ -19,6 +26,7 @@ IF EXIST Prospect-installers (
     rd /s /q Prospect-installers
 )
 
+
 IF EXIST Prospect-win32-x64 (
     color 0C
     echo Removendo a pasta Prospect-win32-x64...
@@ -26,10 +34,17 @@ IF EXIST Prospect-win32-x64 (
     rd /s /q Prospect-win32-x64
 )
 
+IF EXIST .wwebjs_auth (
+    color 0C
+    echo Removendo a pasta .wwebjs_auth...
+    color 07
+    rd /s /q .wwebjs_auth
+)
+
 color 0A
 echo Compilando o aplicativo Prospect...
 color 07
-electron-packager . --icon=src/assets/icon.ico && node build
+electron-builder -p always
 
 color 0A
 echo Compilação concluída com sucesso!
