3576b7a9dccb1bcafc2f0eb5518d9dd55dfff51c
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-03-04 00:42:49 -0500

    [added] debian config

--- build.bat
+++ build.bat
@@ -34,6 +34,13 @@ IF EXIST Prospect-win32-x64 (
     rd /s /q Prospect-win32-x64
 )
 
+IF EXIST linux-builds (
+    color 0C
+    echo Removendo a pasta linux-builds...
+    color 07
+    rd /s /q linux-builds
+)
+
 IF EXIST .wwebjs_auth (
     color 0C
     echo Removendo a pasta .wwebjs_auth...
@@ -41,10 +48,37 @@ IF EXIST .wwebjs_auth (
     rd /s /q .wwebjs_auth
 )
 
+
 color 0A
-echo Compilando o aplicativo Prospect...
+echo Selecione uma opção:
 color 07
-npm run package
+echo 1 - Buildar para Windows
+echo 2 - Buildar para Linux
+echo 3 - Buildar para todos
+choice /c 123 /n /m "Digite o número da opção desejada: "
+set opcao=%errorlevel%
+
+IF %opcao%==1 (
+    color 0A
+    echo Compilando o aplicativo Prospect para Windows...
+    color 07
+    electron-packager . --icon=src/assets/icon.ico && node build
+) ELSE IF %opcao%==2 (
+    color 0A
+    echo Compilando o aplicativo Prospect para Linux...
+    color 07
+    electron-packager . --overwrite --platform=linux --arch=x64 --icon=src/assets/icon.png --prune=true --out=linux-builds
+) ELSE IF %opcao%==3 (
+    color 0A
+    echo Compilando o aplicativo Prospect para todas as plataformas...
+    color 07
+    electron-packager . --icon=src/assets/icon.ico && node build
+    electron-packager . --overwrite --platform=linux --arch=x64 --icon=src/assets/icon.png --prune=true --out=linux-builds
+) ELSE (
+    color 0C
+    echo Opção inválida!
+)
+
 
 color 0A
 echo Compilação concluída com sucesso!
